fc8ca06bb0d483d3bd062503f18d0d4918230c15
arvindayal <arvinddayal@gmail.com>
2014-01-28 16:27:00 -0700

    markers adding and removing

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
 $(function() {
 
 	$('#map').click(function(e) {
-		var newImage = $('<img id="marker" src="marker.png">');
-		$('#main').append(newImage);
 		var evt = e ? e:window.event;
-		var clickX=0, clickY=0;
+		var clickX=0;
+		var clickY=0;
 		if (evt.pageX || evt.pageY) {
 			clickX = evt.pageX;
 			clickY = evt.pageY;
 		}
-		
+		var newImage = $('<img id="marker" src="marker.png">').css({"left": clickX-16, "top": clickY-4});
+		$('body').prepend(newImage);
 		console.log(clickX, clickY);
 	});
 
-
-
-
-
-
-
-
-
-
-
-
+	$(document).on("click","#marker",function() {
+	$(this).remove();
+	});
 
 
 
